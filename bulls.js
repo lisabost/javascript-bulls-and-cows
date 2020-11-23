3a3a4585c7851448cc3de95da51c6bebd645193c
lisabost <lbost@my.wctc.edu>
2020-11-23 10:58:40 -0600

    Initial commit

---
+++ bulls.js
@@ -0,0 +1,198 @@
+$(document).ready(function () {
+
+
+        //computer numbers need to be accessible to all our functions
+        var num1;
+        var num2;
+        var num3;
+        var num4;
+
+        // object containing the validation rules
+        var gameRules =
+            {
+                guess: {
+                    required: true,
+                    digits: true,
+                    minlength: 4,
+                    maxlength: 4
+                }
+            }
+
+        // object containing the error messages
+        var gameMessages =
+            {
+                guess: {
+                    required: "Enter 4 digits from 1-9",
+                    digits: "Please enter 4 digits",
+                    minlength: "Enter 4 digits",
+                    maxlength: "Enter 4 digits"
+                }
+            }
+
+        // event handlers
+        gameSetup();
+
+        //when the new game button is pushed, run game setup
+        $("#newGameButton").click(gameSetup);
+
+        // pass the configuration to the form's validate() method
+        // needs submitHandler, rules and message properties
+        $("form").validate(
+            {
+                submitHandler: runMyGame,
+                rules: gameRules,
+                messages: gameMessages,
+            });
+
+        $("#showRules").click(function() {
+            $("#rules").toggle();
+        });
+        $("#reveal").click(function() {
+            $("#cheat").toggle();
+        });
+
+
+
+        //other functions
+        function gameSetup() {
+            //computer generates a set of 4 random numbers between 1 and 9
+            //checks to make sure no numbers are duplicated
+            num1 = Math.floor(Math.random() * 9) + 1;
+            num2 = Math.floor(Math.random() * 9) + 1;
+            num3 = Math.floor(Math.random() * 9) + 1;
+            num4 = Math.floor(Math.random() * 9) + 1;
+
+            while (num2 === num1) {
+                num2 = Math.floor(Math.random() * 9) + 1;
+            }
+
+            while (num3 === num1 || num3 === num2) {
+                num3 = Math.floor(Math.random() * 9) + 1;
+            }
+
+            while (num4 === num1 || num4 === num2 || num4 === num3) {
+                num4 = Math.floor(Math.random() * 9) + 1;
+            }
+            //sets up our computer number in the cheating div
+            var revealNumber = "" + num1 + "" + num2 + "" + num3 + "" + num4;
+            $("#cheat").text(`You cheated! The answer was ${revealNumber}`);
+
+            //if the guess box and guess submission buttons are toggled off, turn them back on
+            $("#guess:hidden").toggle();
+            $("#guessButton:hidden").toggle();
+
+            //reset text box
+            $("input:text").val("");
+
+            //hide the new game button if it is visible
+            $("#newGameButton:visible").toggle();
+
+            //reset the num guessed div to contain an empty string
+            $("#numGuessed").text("");
+
+            //reset the past guesses to contain empty list
+            $("#pastGuesses li").remove();
+        }
+
+
+        function runMyGame() {
+
+            //get player's guess  from the form
+            var guess = $("#guess").val();
+
+            //make them ints so we can compare easily with the computer's numbers
+            var guess1 = parseInt(guess.charAt(0));
+            var guess2 = parseInt(guess.charAt(1));
+            var guess3 = parseInt(guess.charAt(2));
+            var guess4 = parseInt(guess.charAt(3));
+
+            var guessResult = "";
+            var pastGuess = guess;
+            var cow = "🐄";
+            var bull = "🐂";
+
+            //make sure the player did not enter the same number more than 1 time
+            if (guess1 === guess2 || guess1 === guess3 || guess1 === guess4) {
+                $("#error").show();
+            } else if (guess2 === guess3 || guess2 === guess4) {
+                $("#error").show();
+            } else if (guess3 === guess4) {
+                $("#error").show();
+            } else {
+                $("#error").hide();
+                //compare guesses with computer's numbers and display results
+                if (guess1 === num1) {
+                    //display bull
+                    guessResult += bull;
+                } else if (guess1 === num2 || guess1 === num3 || guess1 === num4) {
+                    //display cow
+                    guessResult += cow;
+                } else {
+                    guessResult += "";
+                }
+
+                if (guess2 === num2) {
+                    //display bull
+                    guessResult += bull;
+                } else if (guess2 === num1 || guess2 === num3 || guess2 === num4) {
+                    //display cow
+                    guessResult += cow;
+                } else {
+                    //display no numbers match
+                    guessResult += "";
+                }
+
+                if (guess3 === num3) {
+                    //display bull
+                    guessResult += bull;
+                } else if (guess3 === num1 || guess3 === num2 || guess3 === num4) {
+                    //display cow
+                    guessResult += cow;
+                } else {
+                    //display no numbers match
+                    guessResult += "";
+                }
+
+                if (guess4 === num4) {
+                    //display bull
+                    guessResult += bull;
+                } else if (guess4 === num1 || guess4 === num2 || guess4 === num3) {
+                    //display cow
+                    guessResult += cow;
+                } else {
+                    //display no numbers match
+                    guessResult += "";
+                }
+
+                //put guess results in div with id of results
+                if (guessResult === "") {
+                    guessResult = "No numbers match";
+                    $("#numGuessed").text(`${guessResult}`)
+                } else {
+                    $("#numGuessed").text(`${guessResult}`);
+                }
+                var displayText = (`${pastGuess} - ${guessResult}`);
+
+                //put guess and results in unordered list in div with id pastGuesses
+                $("#pastGuesses ul").append($("<li>").text(displayText));
+
+                if (guessResult === "🐂🐂🐂🐂") {
+                    //game over, keep showing their guess and display victory message
+                    $("#numGuessed").text(`You guessed it! The number was ${pastGuess}`)
+                    //reset the text box
+                    $("input:text").val("");
+                    //turn off the ability to keep guessing
+                    $("#guess").toggle();
+                    $("#guessButton").toggle();
+                    //turn on the ability to start a new game
+                    $("#newGameButton").toggle();
+                } else {
+                    //reset the text box
+                    $("input:text").val("");
+                }
+            }
+
+        }
+
+    }
+);
